541c661c869297da71d0e375d5c7f1f236224ffc
Ankit <ankitchadda285@gmail.com>
2022-07-07 19:06:46 +0530

    Booking Updated

--- src/Third/Booking.tsx
+++ src/Third/Booking.tsx
@@ -3,7 +3,7 @@ import "./Booking.css";
 import Avatar from "@mui/material/Avatar";
 import Logo from "../image/LabtinLogo.png";
 import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
-import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import Snackbar, { SnackbarOrigin } from "@mui/material/Snackbar";
 import { useNavigate } from "react-router-dom";
@@ -36,6 +36,7 @@ const Booking = () => {
   const toggleOptionsV = (e: any) => {
     if (e.target.parentElement.classList[0] === "appointmentSection") {
       e.target.parentElement.nextSibling.classList.toggle("hideOptions");
+      e.target.parentElement.children[1].classList.toggle("rotate");
     }
   };
 
@@ -86,17 +87,24 @@ const Booking = () => {
           <div className="addedAvatar">
             <Avatar
               alt="Logo"
-              src={Logo}
-              sx={{ width: "4rem", height: "4rem", border: "1px solid black" }}
-            />
-            <h3>Logo</h3>
+              // src={Logo}
+              sx={{
+                width: "4rem",
+                height: "4rem",
+                border: "1px solid grey",
+                bgcolor: "purple",
+              }}
+            >
+              S
+            </Avatar>
+            <h3>Surya</h3>
           </div>
         </div>
       </div>
       <div className="pickUpLocation">
         <h3>Pick Up Location</h3>
         <div className="iconLoaction">
-          <LocationOnRoundedIcon fontSize="inherit" color="inherit" />
+          <LocationOnIcon fontSize="inherit" color="inherit" />
           <p>Let us know where to collect test sample from</p>
         </div>
 
@@ -105,78 +113,86 @@ const Booking = () => {
 
       <div className="selectBookingSlot">
         <h3>Select Booking Slot</h3>
-        <input
-          className="appointmentDate"
-          type="date"
-          name="appointmentDate"
-          onChange={handleDate}
-        />
-        <div className="appointmentSelect">
-          <div className="appointmentSection" onClick={toggleOptionsV}>
-            <h4>
-              Morning <span>(6:00 AM - 12:00 PM)</span>
-            </h4>
-            <ArrowDropDownIcon className="downArrow" fontSize="large" />
+        <div className="selectBookingSlot_div">
+          <input
+            className="appointmentDate"
+            type="date"
+            name="appointmentDate"
+            onChange={handleDate}
+          />
+          <div className="appointmentSelect">
+            <div className="appointmentSection" onClick={toggleOptionsV}>
+              <h4>
+                Morning{" "}
+                <span style={{ fontSize: "80%", fontWeight: "600" }}>
+                  (6:00 AM - 12:00 PM)
+                </span>
+              </h4>
+              <ArrowDropDownIcon className="downArrow" fontSize="large" />
+            </div>
+            <div className="appointmentOptions hideOptions">
+              <div className="appointmentOption" onClick={handleAppointment}>
+                6:00 AM - 7:00 AM
+              </div>
+              <div className="appointmentOption" onClick={handleAppointment}>
+                7:00 AM - 8:00 AM
+              </div>
+              <div className="appointmentOption" onClick={handleAppointment}>
+                8:00 AM - 9:00 AM
+              </div>
+              <div className="appointmentOption" onClick={handleAppointment}>
+                9:00 AM - 10:00 AM
+              </div>
+              <div className="appointmentOption" onClick={handleAppointment}>
+                10:00 AM - 11:00 AM
+              </div>
+              <div className="appointmentOption" onClick={handleAppointment}>
+                11:00 AM - 12:00 PM
+              </div>
+            </div>
           </div>
-          <div className="appointmentOptions hideOptions">
-            <div className="appointmentOption" onClick={handleAppointment}>
-              6:00 AM - 7:00 AM
+          <div className="appointmentSelect">
+            <div className="appointmentSection" onClick={toggleOptionsV}>
+              <h4>
+                Afternoon{" "}
+                <span style={{ fontSize: "80%", fontWeight: "600" }}>
+                  (12:00 PM-2:00PM)
+                </span>
+              </h4>
+              <ArrowDropDownIcon className="downArrow" fontSize="large" />
             </div>
-            <div className="appointmentOption" onClick={handleAppointment}>
-              7:00 AM - 8:00 AM
-            </div>
-            <div className="appointmentOption" onClick={handleAppointment}>
-              8:00 AM - 9:00 AM
-            </div>
-            <div className="appointmentOption" onClick={handleAppointment}>
-              9:00 AM - 10:00 AM
-            </div>
-            <div className="appointmentOption" onClick={handleAppointment}>
-              10:00 AM - 11:00 AM
-            </div>
-            <div className="appointmentOption" onClick={handleAppointment}>
-              11:00 AM - 12:00 PM
-            </div>
-          </div>
-        </div>
-        <div className="appointmentSelect">
-          <div className="appointmentSection" onClick={toggleOptionsV}>
-            <h4>
-              Afternoon <span>(12:00 PM-2:00PM)</span>
-            </h4>
-            <ArrowDropDownIcon fontSize="large" />
-          </div>
 
-          <div className="appointmentOptions hideOptions">
-            <div className="appointmentOption" onClick={handleAppointment}>
-              6:00 AM - 7:00 AM
-            </div>
-            <div className="appointmentOption" onClick={handleAppointment}>
-              7:00 AM - 8:00 AM
-            </div>
-            <div className="appointmentOption" onClick={handleAppointment}>
-              8:00 AM - 9:00 AM
-            </div>
-            <div className="appointmentOption" onClick={handleAppointment}>
-              9:00 AM - 10:00 AM
-            </div>
-            <div className="appointmentOption" onClick={handleAppointment}>
-              10:00 AM - 11:00 AM
-            </div>
-            <div className="appointmentOption" onClick={handleAppointment}>
-              11:00 AM - 12:00 PM
+            <div className="appointmentOptions hideOptions">
+              <div className="appointmentOption" onClick={handleAppointment}>
+                6:00 AM - 7:00 AM
+              </div>
+              <div className="appointmentOption" onClick={handleAppointment}>
+                7:00 AM - 8:00 AM
+              </div>
+              <div className="appointmentOption" onClick={handleAppointment}>
+                8:00 AM - 9:00 AM
+              </div>
+              <div className="appointmentOption" onClick={handleAppointment}>
+                9:00 AM - 10:00 AM
+              </div>
+              <div className="appointmentOption" onClick={handleAppointment}>
+                10:00 AM - 11:00 AM
+              </div>
+              <div className="appointmentOption" onClick={handleAppointment}>
+                11:00 AM - 12:00 PM
+              </div>
             </div>
           </div>
+          <Snackbar
+            sx={{ marginBottom: "5rem" }}
+            anchorOrigin={{ vertical, horizontal }}
+            open={open}
+            onClose={handleClose}
+            message="Slot Selected"
+            action={action}
+            key={vertical + horizontal}
+          />
         </div>
-        <Snackbar
-          sx={{ marginBottom: "5rem" }}
-          anchorOrigin={{ vertical, horizontal }}
-          open={open}
-          onClose={handleClose}
-          message="Slot Selected"
-          action={action}
-          key={vertical + horizontal}
-        />
       </div>
     </div>
   );
